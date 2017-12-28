42375e12c5d9e5d36647c97a9ed78ab9cc3bf54a
wolf4j <rxl5555555@qq.com>
2017-12-28 13:54:32 +0800

    add arrays.go for project
    the array's use for golang

---
+++ go1.9/arrays.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [5]int
+	fmt.Println("emp: ",a) //emp:  [0 0 0 0 0]
+
+	a[4] = 1000
+	fmt.Println("set:",a)
+	fmt.Println("get:",a[4])
+
+	b := [5]int{1,2,3,4,5}
+	fmt.Println("dcl:",b)
+
+	var twoArr [2][3]int
+	for i := 0 ; i < 2 ; i++ {
+		for j := 0 ; j < 3 ; j++ {
+			twoArr[i][j] = i + j
+		}
+	}
+	fmt.Println(twoArr)
+}
