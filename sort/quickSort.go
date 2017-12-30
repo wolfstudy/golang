a94fab7fcafcecd6b78208483a6c1a0f71bb073f
wolf4j <rxl5555555@qq.com>
2017-12-30 15:34:33 +0800

    wolf4j
    use go language achieve quick sort for project

---
+++ sort/quickSort.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+	var sortArray = []int{3, 41, 24, 76, 11, 45, 3, 3, 64, 21, 69, 19, 36}
+	fmt.Println(sortArray)
+	qSort(sortArray, 0, len(sortArray)-1)
+	fmt.Println(sortArray)
+}
+
+func qSort(array []int, low, high int) {
+	if low < high {
+		m := partition(array, low, high)
+		// fmt.Println(m)
+		qSort(array, low, m-1)
+		qSort(array, m+1, high)
+	}
+}
+
+func partition(array []int, low, high int) int {
+	key := array[low]
+	tmpLow := low
+	tmpHigh := high
+	for {
+		//查找小于等于key的元素，该元素的位置一定是tmpLow到high之间，因为array[tmpLow]及左边元素小于等于key，不会越界
+		for array[tmpHigh] > key {
+			tmpHigh--
+		}
+		//找到大于key的元素，该元素的位置一定是low到tmpHigh+1之间。因为array[tmpHigh+1]必定大于key
+		for array[tmpLow] <= key && tmpLow < tmpHigh {
+			tmpLow++
+		}
+
+		if tmpLow >= tmpHigh {
+			break
+		}
+		// swap(array[tmpLow], array[tmpHigh])
+		array[tmpLow], array[tmpHigh] = array[tmpHigh], array[tmpLow]
+		fmt.Println(array)
+	}
+	array[tmpLow], array[low] = array[low], array[tmpLow]
+	return tmpLow
+}
