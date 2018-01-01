3aa257eaaf9be413d02b34d5e36451159f9b2d99
wolf4j <rxl5555555@qq.com>
2018-01-01 10:09:25 +0800

    shell sort
    add shell sort for poject

---
+++ sort/shellSort.go
@@ -0,0 +1,26 @@
+package main
+
+func swap1 (i, j int,a []int)  {
+	a[i], a[j] = a[j], a[i]
+}
+
+func ShellSort(a []int){
+	n := len(a)
+	
+	h := 1
+	for h < n/3 {
+		h = 3*h +1  //寻找合适的时间间隔h
+	}
+
+	for h >= 1 {
+		//将数组变为间隔h个元素有序
+		for i := h; i < n; i++ {
+			//间隔h插入排序
+			for j := i; j >= h && a[j] < a[j - h] ; j -= h {
+				swap1(j, j-h, a)
+			}
+		}
+
+		h /= 3
+	}
+}
