9681c8aacb5ee5ba935da6f57c8f258fc1c88b5a
wolf4j <rxl5555555@qq.com>
2018-01-10 09:45:21 +0800

    insert func
    add insert func for project

--- sort/insertSort.go
+++ sort/insertSort.go
@@ -1,6 +1,15 @@
 package main
 
-func main() {
-
+func insertSort(nums []int)  {
+	for i := 1; i < len(nums); i++ {
+		if nums[i] < nums[i-1] {
+			j := i - 1
+			temp := nums[i]
+			for j >= 0 && nums[j] > temp {
+				nums[j+1] = nums[j]
+				j--
+			}
+			nums[j+1] = temp
+		}
+	}
 }
-
