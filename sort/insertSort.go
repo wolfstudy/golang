d5ee8a64d6d6634f04c93373c9a25685a1fbe446
wolf4j <rxl5555555@qq.com>
2018-01-10 09:50:53 +0800

    main
    add main func ,test

--- sort/insertSort.go
+++ sort/insertSort.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func insertSort(nums []int)  {
 	for i := 1; i < len(nums); i++ {
 		if nums[i] < nums[i-1] {
@@ -12,4 +14,11 @@ func insertSort(nums []int)  {
 			nums[j+1] = temp
 		}
 	}
+}
+
+func main() {
+	nums := []int{3,6,1,5,7,2,8,4,9}
+	fmt.Println("初始状态：",nums)
+	insertSort(nums)
+	fmt.Println("排序后状态：",nums)
 }
