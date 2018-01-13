c77caa7b18285bf690c9fbf828e01ffee38bb4a9
wolf4j <rxl5555555@qq.com>
2018-01-13 22:36:55 +0800

    main
    test main func

--- tools/c10k.go
+++ tools/c10k.go
@@ -4,10 +4,26 @@ import (
 	"net"
 	"fmt"
 	"time"
+	"log"
 )
 
 //golang solution c10k question.
-func handle (conn net.Conn) {
+func handle(conn net.Conn) {
 	fmt.Fprintf(conn, "%s", time.Now().String())
 	conn.Close()
+}
+
+func main() {
+	l, err := net.Listen("tcp", "8080")
+	if err!=nil {
+		log.Fatal(err)
+	}
+
+	for  {
+		conn,err:= l.Accept()
+		if err!=nil {
+			log.Fatal(err)
+		}
+		go handle(conn)
+	}
 }
