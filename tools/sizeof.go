194ba054be94e75501a37439ec56b4095f422a9b
wolf4j <rxl5555555@qq.com>
2018-01-17 15:47:35 +0800

    sizeof
    sizeof

---
+++ tools/sizeof.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	fmt.Println(unsafe.Sizeof(true))
+	fmt.Println(unsafe.Sizeof(int8(0)))
+	fmt.Println(unsafe.Sizeof(int16(10)))
+	fmt.Println(unsafe.Sizeof(int32(10000000)))
+	fmt.Println(unsafe.Sizeof(int64(10000000000000)))
+	fmt.Println(unsafe.Sizeof(int(10000000000000000)))
+}
