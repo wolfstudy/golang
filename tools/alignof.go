8bf3f1a5c13dd631054efe7a27464b1a2646d690
wolf4j <rxl5555555@qq.com>
2018-01-17 15:49:14 +0800

    alignof
    alignof

---
+++ tools/alignof.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	var b bool
+	var i8 int8
+	var i16 int16
+	var i64 int64
+	var f32 float32
+	var s string
+	var m map[string]string
+	var p *int32
+	fmt.Println(unsafe.Alignof(b))
+	fmt.Println(unsafe.Alignof(i8))
+	fmt.Println(unsafe.Alignof(i16))
+	fmt.Println(unsafe.Alignof(i64))
+	fmt.Println(unsafe.Alignof(f32))
+	fmt.Println(unsafe.Alignof(s))
+	fmt.Println(unsafe.Alignof(m))
+	fmt.Println(unsafe.Alignof(p))
+}
