2fd6c2ac8b504b010e04b9d952738cd44a1381c9
wolf4j <rxl5555555@qq.com>
2018-01-17 15:43:04 +0800

    type
    reflect type

--- tools/reflect/reflect.go
+++ tools/reflect/reflect.go
@@ -10,10 +10,8 @@ import (
 //标准库为我们提供两种类型来分别表示他们reflect.Value和reflect.Type，
 // 并且提供了两个函数来获取任意对象的Value和Type。
 
-
-
 func main() {
-	u := User{"make",20}
+	u := User{"make", 20}
 
 	//通过reflect.ValueOf函数把任意类型的对象转为一个reflect.Value
 
@@ -28,11 +26,16 @@ func main() {
 	//将上述过程逆转回来
 
 	//reflect.Value为我们提供了Inteface方法来帮我们做这个事情
-	u1:= v.Interface().(User)
+	u1 := v.Interface().(User)
 	fmt.Println(u1)
 
-	
+	//这里可以还原的原因是因为在Go的反射中，把任意一个对象分为reflect.Value和reflect.Type，
+	// 而reflect.Value又同时持有一个对象的reflect.Value和reflect.Type,
+	// 所以我们可以通过reflect.Value的Interface方法实现还原。
 
+	//如何从一个reflect.Value获取对应的reflect.Type。
+	t1 := v.Type()
+	fmt.Println(t1)
 }
 
 type User struct {
