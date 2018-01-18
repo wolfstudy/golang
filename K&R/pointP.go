0a736fb0d8a71ebb41d15ac16fede327f038a623
wolf4j <rxl5555555@qq.com>
2018-01-18 13:52:10 +0800

    point
    point

---
+++ K&R/pointP.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	x1 := 1
+	p := &x1
+	fmt.Println(*p)
+	fmt.Println(p) // 打印的是否是内存地址，还是一个hashcode
+
+	*p = 2
+	fmt.Println(x1)
+
+	var x, y int
+	fmt.Println(&x == &x, &x == &y, &x == nil)
+
+	//icpr 需要记住一个点，虽然是指针自增，但是增加的是这个指针指向的值，这个指针不自己跑
+	v := 1
+	res := icpr(&v)
+	fmt.Println("===============icpr============\n",res)
+
+}
+
+func icpr(p *int) int {
+	*p++
+	return *p
+}
