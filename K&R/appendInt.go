0478981cdff3e52a9a32e1af6df5a4532c452f5b
wolf4j <rxl5555555@qq.com>
2018-01-19 11:55:53 +0800

    fix append
    fix append

--- K&R/appendInt.go
+++ K&R/appendInt.go
@@ -2,15 +2,18 @@ package main
 
 import "fmt"
 
+//appendInt 一次只能追加一个元素
+
 func main() {
 	var x, y []int
-	for i := 0; i <= 10; i++ {
+	for i := 0; i < 10; i++ {
 		y = appendInt(x, i)
 		fmt.Printf("%d,cap=%d\t%v\n", i, cap(y), y)
 		x = y
 	}
 }
 
+//追加一个元素
 func appendInt(x []int, y int) []int {
 	var z []int
 
@@ -19,7 +22,7 @@ func appendInt(x []int, y int) []int {
 		z = x[:zLen]
 	} else {
 		zCap := zLen
-		if zCap <= 2*len(x) {
+		if zCap < 2*len(x) {
 			zCap = 2 * len(x)
 		}
 		z = make([]int, zLen, zCap)
@@ -27,6 +30,30 @@ func appendInt(x []int, y int) []int {
 	}
 
 	z[len(x)] = y
+
+	return z
+}
+
+//拓展 ...int表示接收变长的slice
+func appendInt1(x []int, y ...int) []int {
+	var z []int
+
+	//fix
+	zLen := len(x) + len(y)
+
+	if zLen <= cap(x) {
+		z = x[:zLen]
+	} else {
+		zCap := zLen
+		if zCap < 2*len(x) {
+			zCap = 2 * len(x)
+		}
+		z = make([]int, zLen, zCap)
+		copy(z, x)
+	}
+
+	//fix
+	z[len(x):] = y
 
 	return z
 }
