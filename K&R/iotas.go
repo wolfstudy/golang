25bc0932081041f80acb38c217ac2d9125c64e35
wolf4j <rxl5555555@qq.com>
2018-01-19 11:43:26 +0800

    iota
    iota

--- K&R/iotas.go
+++ K&R/iotas.go
@@ -4,22 +4,43 @@ import "fmt"
 
 //iota 只能在常量（const）中使用
 func main() {
+	//const (
+	//	a = iota
+	//	b
+	//	c
+	//	d = iota
+	//	e
+	//	_
+	//	_
+	//	f
+	//)
+	//
+	//fmt.Println("a = %v", a)
+	//fmt.Println("b = %v", b)
+	//fmt.Println("c = %v", c)
+	//fmt.Println("d = %v", d)
+	//fmt.Println("e = %v", e)
+	//fmt.Println("f = %v", f)
+
 	const (
-		a = iota
-		b
-		c
-		d = iota
-		e
-		_
-		_
-		f
+		_   = 1 << (10 * iota)
+		KiB
+		MiB
+		GiB
+		TiB
+		PiB
+		EiB
+		//ZiB
+		//YiB
 	)
 
-	fmt.Println("a = %v", a)
-	fmt.Println("b = %v", b)
-	fmt.Println("c = %v", c)
-	fmt.Println("d = %v", d)
-	fmt.Println("e = %v", e)
-	fmt.Println("f = %v", f)
+	fmt.Println(KiB)
+	fmt.Println(MiB)
+	fmt.Println(GiB)
+	fmt.Println(TiB)
+	fmt.Println(PiB)
+	fmt.Println(EiB)
+	//fmt.Println(ZiB)
+	//fmt.Println(YiB)
 
 }
