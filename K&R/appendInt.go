e49e5f8e5fe8add2ecd0d40f9747e6f6476933fc
wolf4j <rxl5555555@qq.com>
2018-01-19 11:43:16 +0800

    append
    append

---
+++ K&R/appendInt.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x, y []int
+	for i := 0; i <= 10; i++ {
+		y = appendInt(x, i)
+		fmt.Printf("%d,cap=%d\t%v\n", i, cap(y), y)
+		x = y
+	}
+}
+
+func appendInt(x []int, y int) []int {
+	var z []int
+
+	zLen := len(x) + 1
+	if zLen <= cap(x) {
+		z = x[:zLen]
+	} else {
+		zCap := zLen
+		if zCap <= 2*len(x) {
+			zCap = 2 * len(x)
+		}
+		z = make([]int, zLen, zCap)
+		copy(z, x)
+	}
+
+	z[len(x)] = y
+
+	return z
+}
