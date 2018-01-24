30d097f7755b3295fa555a19b2eb8578950b1703
wolf4j <rxl5555555@qq.com>
2018-01-24 09:34:53 +0800

    put
    put

--- tools/noLockQueue_test.go
+++ tools/noLockQueue_test.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"testing"
+	"fmt"
+	"sync/atomic"
+	"time"
+	"runtime"
+	"sync"
 )
 
 var (
@@ -28,4 +33,52 @@ func TestQueue(t *testing.T) {
 	if q := q.Quantity(); q != 0 {
 		t.Errorf("Quantity Error: [%v] <>[%v]", q, 0)
 	}
+}
+
+func testQueuePutGet(t *testing.T, grp, cnt int) (
+	put time.Duration, get time.Duration) {
+	var wg sync.WaitGroup
+	var id int32
+	wg.Add(grp)
+	q := NewQueue(1024 * 1024)
+	start := time.Now()
+	for i := 0; i < grp; i++ {
+		go func(g int) {
+			defer wg.Done()
+			for j := 0; j < cnt; j++ {
+				val := fmt.Sprintf("Node.%d.%d.%d", g, j, atomic.AddInt32(&id, 1))
+				ok, _ := q.Put(&val)
+				for !ok {
+					time.Sleep(time.Microsecond)
+					ok, _ = q.Put(&val)
+				}
+			}
+		}(i)
+	}
+	wg.Wait()
+	end := time.Now()
+	put = end.Sub(start)
+
+	wg.Add(grp)
+	start = time.Now()
+	for i := 0; i < grp; i++ {
+		go func() {
+			defer wg.Done()
+			for j := 0; j < cnt; {
+				_, ok, _ := q.Get()
+				if !ok {
+					runtime.Gosched()
+				} else {
+					j++
+				}
+			}
+		}()
+	}
+	wg.Wait()
+	end = time.Now()
+	get = end.Sub(start)
+	if q := q.Quantity(); q != 0 {
+		t.Errorf("Grp:%v, Quantity Error: [%v] <>[%v]", grp, q, 0)
+	}
+	return put, get
 }
