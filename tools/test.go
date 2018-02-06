e7fa7106d7cc7ab925d8b690d43b3941449b92b2
wolf4j <rxl5555555@qq.com>
2018-02-06 20:31:26 +0800

    add
    add

---
+++ tools/test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.MaxInt64)
+	i := 9223372036854775807 + 1
+	fmt.Println("是否会报错呢？")
+	fmt.Println(i)
+}
+
+/**
+
+ */
+func add(a,b int)  {
+
+}
