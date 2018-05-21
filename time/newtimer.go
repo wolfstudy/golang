8b3763ce3e9139ef7ef397cfc5963dd7ea7dc7bd
wolf4j <rxl5555555@qq.com>
2018-05-21 14:57:01 +0800

    add time
    add time

---
+++ time/newtimer.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+func main() {
+	//初始化定时器
+	t := time.NewTimer(2 * time.Second) //返回一个time的指针
+
+	//当前时间
+	now := time.Now()
+	fmt.Println("now time: %v", now)
+
+	expire := <-t.C
+	fmt.Printf("Expiration time: %v.\n", expire)
+}
